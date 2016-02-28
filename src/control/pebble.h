af5dabb99c5200cc7b98a7ad57de618fba7b7bac
Victor Pineda <vpineda1996@gmail.com>
2016-02-27 22:10:33 -0800

    applied some debug flags on compilation time for testing

--- src/control/pebble.h
+++ src/control/pebble.h
@@ -9,11 +9,12 @@
 #ifndef pebble_h
 #define pebble_h
 
-// #include <pebble.h>
+#ifndef DEBUG
+#include <pebble.h>
 
+#else
 // COMMENT ALL OF THE FOLLOWING WHEN COMPILING
 #include <stdlib.h>
 typedef void GContext;
-
 
 #endif /* pebble_h */
